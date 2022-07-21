9f5c368d68788dc298c81e93938d81319e1ab202
Zachary Christensen <zacharychristensen@outlook.com>
2022-07-21 00:39:04 -0500

    Fixed EvilBrickAttackBall ps so is turn off then on to avoid spawn problem

--- Assets/Scripts/EvilBrickAttackBall.cs
+++ Assets/Scripts/EvilBrickAttackBall.cs
@@ -6,9 +6,16 @@ public class EvilBrickAttackBall : MonoBehaviour
 {
     private Rigidbody2D _rb;
 
+    public GameObject _psGameObject;
+
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();    
+    }
+
+    private void Start()
+    {
+        _psGameObject.SetActive(true);   
     }
 
     public void Shoot(Vector2 direction)
