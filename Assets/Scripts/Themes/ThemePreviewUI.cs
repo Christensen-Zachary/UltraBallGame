61abd0e73e7bfcfa441c21a246787c8686658c03
Zachary Christensen <zacharychristensen@outlook.com>
2023-01-31 23:41:30 -0600

    fixed bug where closing game during theme preview caused theme to not reset until starting new level

--- Assets/Scripts/Themes/ThemePreviewUI.cs
+++ Assets/Scripts/Themes/ThemePreviewUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -12,6 +13,12 @@ public class ThemePreviewUI : MonoBehaviour
     private void Awake()
     {
         _b4Theme = ES3.Load<ThemeType>(BGStrings.ES_THEMETYPEB4PREVIEW);
+    }
+
+    private IEnumerator Start() 
+    {
+        yield return null;
+        ES3.Save(BGStrings.ES_THEMETYPE, _b4Theme);    
     }
 
 
