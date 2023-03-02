f9d94f21ed8d32ce5320adfbe964c9ff5a78c173
Zachary Christensen <zacharychristensen@outlook.com>
2023-03-01 18:33:13 -0600

    Removed extraneous header from BGExtensions

--- Assets/Scripts/Static/BGExtensions.cs
+++ Assets/Scripts/Static/BGExtensions.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 
 public static class RectTransformExtensions
